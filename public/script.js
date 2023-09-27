87f0e1478a51d1608213eb73114b1d8da255623f
Ner Karso <nerpkarso@gmail.com>
2023-09-26 23:26:24 -0300

    feat: one way video stream

--- public/script.js
+++ public/script.js
@@ -1,66 +1,58 @@
-const socket = io('/');
-const myPeer = new Peer(undefined);
-const peers = {};
+const CASTER_ID = '3d086243-cc7e-4c3a-a060-a5ad27a59726';
+const peerCaster = new Peer(CASTER_ID);
+const peerSubscriber = new Peer();
+const overlay = document.getElementById('overlay');
 
-const videoGrid = document.getElementById('video-grid');
-const myVideo = document.createElement('video');
-myVideo.style.display = 'none';
-myVideo.muted = true;
-
-navigator.mediaDevices
-  .getUserMedia({
-    video: true,
-    audio: false,
-  })
-  .then((stream) => {
-    addVideoStream(myVideo, stream);
-
-    myPeer.on('call', (call) => {
-      call.answer(stream);
-
-      const video = document.createElement('video');
-      video.id = 'user-video';
-
-      call.on('stream', (userVideoStream) => {
-        addVideoStream(video, userVideoStream);
+if (IS_CASTER) {
+  // Hide overlay
+  overlay.style.display = 'none';
+  // Get webcam
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then((stream) => {
+    addVideoStream(stream, true);
+    peerCaster.on('connection', (conn) => {
+      conn.on('open', () => {
+        // Call the subscribers
+        peerCaster.call(conn.peer, stream);
       });
     });
-
-    socket.on('user-connected', (userId) => {
-      connectToNewUser(userId, stream);
-    });
   });
-
-socket.on('user-disconnected', (userId) => {
-  if (peers[userId]) peers[userId].close();
-});
-
-myPeer.on('open', (id) => {
-  socket.emit('join-room', ROOM_ID, id);
-});
-
-function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream);
-  const video = document.createElement('video');
-  video.id = 'user-video';
-
-  call.on('stream', (userVideoStream) => {
-    addVideoStream(video, userVideoStream);
-  });
-
-  call.on('close', () => {
-    video.remove();
-  });
-
-  peers[userId] = call;
 }
 
-function addVideoStream(video, stream) {
+peerSubscriber.on('call', (call) => {
+  call.on('stream', (stream) => {
+    addVideoStream(stream);
+  });
+  if (IS_RECEIVER) {
+    call.answer(null);
+  }
+});
+
+peerSubscriber.on('open', () => {
+  // Connect to the caster so that they will call us
+  peerSubscriber.connect(CASTER_ID);
+});
+
+overlay.addEventListener('click', () => {
+  const video = document.getElementById('#cast');
+  if (video && overlay && IS_RECEIVER) {
+    video.play();
+    overlay.style.display = 'none';
+  }
+});
+
+function addVideoStream(stream, autoplay) {
+  const video = document.createElement('video');
+  video.id = '#cast';
   video.srcObject = stream;
 
   video.addEventListener('loadedmetadata', () => {
-    video.play();
+    if (autoplay) {
+      video.play();
+    }
+    if (overlay) {
+      overlay.innerText = 'Click to play';
+    }
   });
 
-  videoGrid.append(video);
+  document.body.prepend(video);
 }
--- server.js
+++ server.js
@@ -1,30 +1,15 @@
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const { v4: uuidV4 } = require('uuid');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`);
+  res.render('index', { isCaster: false });
 });
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room });
+app.get('/cast', (req, res) => {
+  res.render('index', { isCaster: true });
 });
 
-io.on('connection', (socket) => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId);
-
-    socket.to(roomId).broadcast.emit('user-connected', userId);
-
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId);
-    });
-  });
-});
-
-server.listen(3000);
+app.listen(3000);
